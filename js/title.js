9843f787898b3a55d601f46e2d39b343bf1eeeff
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2022-10-23 21:14:29 -0300

    ahora el titulo de la pagina se mueve

---
+++ js/title.js
@@ -0,0 +1,24 @@
+let titulo = "The Z Warriors";
+let titulo_clon;
+letra = '...........................';
+iteracion = -1;
+document.title = titulo;
+largo_titulo = titulo.length;
+document.title = titulo;
+var time_shooter = null; //por los lol-es
+retraso_ms = 200; //espacio de tiempo entre cambio de letras en milisegundos
+
+function mover_titulo(){
+	if(iteracion < largo_titulo){
+		titulo_clon = letra.substring(0,iteracion+1) + titulo.substring(iteracion+1);
+		//console.log("Primer titulo: "+titulo_clon);
+		iteracion +=1;
+	}
+	else{
+		iteracion = -1;
+	}
+	document.title = titulo_clon;
+	time_shooter = setTimeout("mover_titulo()", retraso_ms);  //llamada recursiva con retraso
+};
+
+mover_titulo();
