19fc03a614f77ec78c571be0bc58e090dd9dbae3
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2023-05-01 15:09:14 -0400

    logica de juego

--- js/gato.js
+++ js/gato.js
@@ -1,100 +1,115 @@
-//variables para el gato
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const button3 = document.getElementById("button3");
-const button4 = document.getElementById("button4");
-const button5 = document.getElementById("button5");
-const button6 = document.getElementById("button6");
-const button7 = document.getElementById("button7");
-const button8 = document.getElementById("button8");
-const button9 = document.getElementById("button9");
-const reset   = document.getElementById("reset");
-const gamezone= document.getElementById("gamezone");
-const winzone = document.getElementById("ganador");
-//---------------------------------
-var x='X';
-var o='O';
-var turno = 1;
-var p1=[];
-var p2=[];
-// Combinaciones victoriosas
-var winners = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+$(document).ready(function(){
+    let turno = 1;
+    let p1=[];
+    let p2=[];
+    const winners = [[1,2,3],[4,5,6],[7,8,9],
+                    [1,4,7],[2,5,8],[3,6,9],
+                    [1,5,9],[3,5,7]];
 
-// Funciones de marcar casillas
-button1.onclick = function(){marca(button1, 1)};
-button2.onclick = function(){marca(button2, 2)};
-button3.onclick = function(){marca(button3, 3)};
-button4.onclick = function(){marca(button4, 4)};
-button5.onclick = function(){marca(button5, 5)};
-button6.onclick = function(){marca(button6, 6)};
-button7.onclick = function(){marca(button7, 7)};
-button8.onclick = function(){marca(button8, 8)};
-button9.onclick = function(){marca(button9, 9)};
+    function yagano(){
+        let ganador;
+        if(turno % 2 == 1 ){
+//            console.log("Player 1 ganó");
+            ganador = $("#player1");
+        }else{
+//            console.log("Player 2 ganó");
+            ganador = $("#player2");
+        }
+        $(ganador).toggleClass("winner");
+        for (let i = 0; i < 3; i++){
+            $(ganador).animate({height: '100px'}, "slow");
+            $(ganador).animate({height: '500px'}, "slow");
+        }
+    };
 
-// Reseteamos el contador de turnos y las variables de las jugadas
-reset.onclick=function(){
-    p1=[];
-    p2=[];
-    turno=1;
-    button1.innerHTML = "&nbsp";
-    button2.innerHTML = "&nbsp";
-    button3.innerHTML = "&nbsp";
-    button4.innerHTML = "&nbsp";
-    button5.innerHTML = "&nbsp";
-    button6.innerHTML = "&nbsp";
-    button7.innerHTML = "&nbsp";
-    button8.innerHTML = "&nbsp";
-    button9.innerHTML = "&nbsp";
-    gato.style.display="none";
-    ///gamezone.classList.toggle('ocultar');
-    banner.style.display="inline-flex";
-    winzone.innerText="";
-};
-
-// agregamos la nueva casilla marcada al jugador actual
-function marca(buttonplayer, casilla){
-    switch(buttonplayer.innerText){
-        case x:
-        case o:
-            break;
-        default:
-            if(turno % 2 == 1 ){
-                buttonplayer.innerText = x;
-                p1.push(casilla);
-                if(p1.length > 2){
-                    ganar(p1);
-                }
-            }else{
-                buttonplayer.innerText = o;
-                p2.push(casilla);
-                if(p2.length > 2){
-                    ganar(p2);
-                }
-            }
-        turno += 1;
-    }
-};
-
-// Comprobamos si el player actual ganó
-function ganar(player){
-    player.sort();
-    for(i=0; i<8; i+=1){
-        if(player.includes(winners[i][0])){
-            if(player.includes(winners[i][1])){
-                if(player.includes(winners[i][2])){
-                    yagano();
+    function ganar(player){
+        player.sort();
+        for(let i=0; i<8; i+=1){
+            if(player.includes(winners[i][0])){
+                if(player.includes(winners[i][1])){
+                    if(player.includes(winners[i][2])){
+                        yagano();
+                    }
                 }
             }
         }
-    }
-};
+    };
+// Hacer que juegue de vuelta
+    function comPlayer(){
 
-// cambiamos la zona de juego completa por el texto de anuncio de ganador
-function yagano(){
-    gamezone.classList.toggle('ocultar');// a esto hay que hacerle toggle
-    if(turno % 2 == 1 ){
-        winzone.innerText="Player 1 ganó";
-    }else{
-        winzone.innerText="Player 2 ganó";
     }
-};
+
+    function cambio(){
+        $("#personajes").toggleClass("oculto");
+        $("#zonaJuego").toggleClass("oculto");
+    }
+// Se selecciona al personaje y pasamos al juego
+    $(".personaje").on({
+        click: function(){
+            // animación de selección
+            // enviar imagen correspondiente a la selección
+            cambio();
+        }
+    });
+// aquí marcamos las jugadas
+    $(".casilla").click(function(){
+        switch($(this).text()){
+            case 'X':
+            case 'O':
+                break;
+            default:
+                if(turno % 2 == 1 ){
+                    p1.push(parseInt($(this).text()));
+                    $(this).text('X');
+                    if(p1.length > 2){
+                        ganar(p1);
+                    }
+                }else{
+                    p2.push(parseInt($(this).text()));
+                    $(this).text('O');
+                    if(p2.length > 2){
+                        ganar(p2);
+                    }
+                }
+                $(this).css("color" , "black");
+            turno += 1;
+        }
+        $("#btnReset").text("Reset Game");
+        console.log("p1: "+p1);
+        console.log("p2: "+p2);
+    });
+// Clickear Reset
+    $("#btnReset").click(function(){
+
+        if( $("#btnReset").text() == "PERSONAJES"){
+            cambio();
+        }else{
+            $("#btnReset").text("PERSONAJES");
+        }
+        let casillas = $(".casilla").toArray();
+        for(var i = 0; i < 9; i++){
+            casillas[i].innerText = i+1;
+            // poner color de letra transparente
+            $(".casilla").css("color" , "transparent");
+        }
+        p1=[];
+        p2=[];
+        turno = 1;
+        $("#player1").removeClass("winner");
+        $("#player2").removeClass("winner");
+        $("#player1").css("height" , "100px");
+        $("#player2").css("height" , "100px");
+        
+    });
+// Animacion Footer
+    $("footer").on({
+        mouseenter: function(){
+            $(this).animate({height: '10vh'}, 2000);
+            $(".disclaimer").animate({fontSize:'3rem'}, 2000);
+        },
+          click: function(){
+            $(this).animate({height: '4vh'}, 800);
+            $(".disclaimer").animate({fontSize:'0rem'}, 1000);
+        }
+    });
+});
--- js/title.js
+++ js/title.js
@@ -1,23 +1,23 @@
-let titulo = "The Z Warriors";
-let titulo_clon;
-letra = '...........................';
-iteracion = -1;
-document.title = titulo;
-largo_titulo = titulo.length;
-document.title = titulo;
-var time_shooter = null; //por los lol-es
-retraso_ms = 500; //espacio de tiempo entre cambio de letras en milisegundos
+$(document).ready(function(){
+	let titulo = "The Z Warriors";
+	let titulo_clon;
+	letra = '...........................';
+	iteracion = -1;
+	document.title = titulo;
+	largo_titulo = titulo.length;
+	document.title = titulo;
+	var time_shooter = null; //por los lol-es
+	retraso_ms = 500; //espacio de tiempo entre cambio de letras en milisegundos
 
-function mover_titulo(){
-	if(iteracion < largo_titulo){
-		titulo_clon = letra.substring(0,iteracion+1) + titulo.substring(iteracion+1);
-		iteracion +=1;
-	}
-	else{
-		iteracion = -1;
-	}
-	document.title = titulo_clon;
-	time_shooter = setTimeout("mover_titulo()", retraso_ms);  //llamada recursiva con retraso
-};
-
-window.addEventListener('load', mover_titulo)
+	function mover_titulo(){
+		if(iteracion < largo_titulo){
+			titulo_clon = letra.substring(0,iteracion+1) + titulo.substring(iteracion+1);
+			iteracion +=1;
+		}
+		else{
+			iteracion = -1;
+		}
+		document.title = titulo_clon;
+		time_shooter = setTimeout("mover_titulo()", retraso_ms);  //llamada recursiva con retraso
+	};
+});
