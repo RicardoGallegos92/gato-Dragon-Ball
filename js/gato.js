aa9e09ed1f824340b031ac80c68ac680c2f0df69
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2023-05-19 01:52:43 -0400

    correccion animacion y diseño, seleccion COM

--- js/gato.js
+++ js/gato.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    let positionName, positionPersonaje;
     let turno = 1;
-    let p1=[];
-    let p2=[];
+    let p1=[],
+        p2=[];
+    let ganador = null,
+        perdedor = null;
     const winners = [[1,2,3],[4,5,6],[7,8,9],
                     [1,4,7],[2,5,8],[3,6,9],
                     [1,5,9],[3,5,7]];
 
     function yagano(){
-        let ganador, perdedor;
         if(turno % 2 == 1 ){
 //            console.log("Player 1 ganó");
             ganador = $("#player1");
@@ -18,10 +18,11 @@ $(document).ready(function(){
             ganador = $("#player2");
             perdedor = $("#player1");
         }
-        $(ganador).toggleClass("winner");
+        $(ganador).addClass("winner");
+        $(perdedor).addClass("oculto");
         for (let i = 0; i < 3; i++){
-            $(ganador).animate({height: '100px'}, "slow");
-            $(ganador).animate({height: '500px'}, "slow");
+            $(ganador).animate({height: '100px'}, 300);
+            $(ganador).animate({height: '500px'}, 300);
         }
     };
 
@@ -39,50 +40,22 @@ $(document).ready(function(){
     };
 // Hacer que juegue de vuelta
     function comPlayer(){
-        // primera jugada al azar
+        // primera jugada al azar if(turno < 3)
         // luego se usa la discriminación de ganar()
-        //* para elegir una de las posibles jugadas
+        // *para elegir una de las posibles jugadas
+        // *según las cantidad de coincidencias
         // evaluar si la casilla está disponible
         // *-repetir proceso
         // en paralelo revisar las posibilidades
-        //* de ganar que tiene el jugador y priorizar bloquearlo
+        // *de ganar que tiene el jugador y priorizar bloquearlo
     }
 
     function cambio(){
-        $("#personajes").toggleClass("oculto");
-        $("#zonaJuego").toggleClass("oculto");
+        $("#personajes").removeClass("oculto");
+        $("#zonaJuego").addClass("oculto");
     }
-// Se selecciona al personaje y pasamos al juego
-    $(".personaje").on({
-        click: function(){
-            // animación de selección
-            // enviar imagen correspondiente a la selección
-            cambio();
-        },
-        mouseenter: function(){
-            positionPersonaje = $(this).position();
-            positionName = $(this).next().position();
 
-            $(this).next().children().css({"color" : "yellow",
-                            "position" : "absolute",
-                            "-webkit-text-stroke-color" : "rgb(0, 0, 0)",
-                            "-webkit-text-stroke-width" : "1.75px"});
-            
-            $(this).next().children().animate({top : positionPersonaje.top-50,
-                                                left : positionPersonaje.left,
-                                                fontSize: "5rem",}, 500, "swing");
-        },
-        mouseleave: function(){
-            $(this).next().children().animate({top : positionName.top,
-                                                left : positionName.left,
-                                                fontSize: "0rem"}, 500, "swing",
-                                            function(){
-                                                $(this).removeAttr('style');
-                                            }
-            );
-            //detener toda animación !!!!!!!!
-        }
-    });
+
 // aquí marcamos las jugadas
     $(".casilla").click(function(){
         switch($(this).text()){
@@ -91,12 +64,14 @@ $(document).ready(function(){
                 break;
             default:
                 if(turno % 2 == 1 ){
+                    console.log("Pushea p1");
                     p1.push(parseInt($(this).text()));
                     $(this).text('X');
                     if(p1.length > 2){
                         ganar(p1);
                     }
                 }else{
+                    console.log("Pushea p2");
                     p2.push(parseInt($(this).text()));
                     $(this).text('O');
                     if(p2.length > 2){
@@ -104,19 +79,22 @@ $(document).ready(function(){
                     }
                 }
                 $(this).css("color" , "black");
-            turno += 1;
+                //console.log("\nturno: "+turno);
+                turno += 1;
         }
         $("#btnReset").text("Reset Game");
-//        console.log("p1: "+p1);
-//        console.log("p2: "+p2);
+        console.log("p1: "+p1);
+        console.log("p2: "+p2);
     });
 // Clickear Reset
     $("#btnReset").click(function(){
-
-        if( $("#btnReset").text() == "PERSONAJES"){
+        if (ganador != null){
+            $(ganador).stop(true, true);
+        }
+        if( $(this).text() == "PERSONAJES"){
             cambio();
         }else{
-            $("#btnReset").text("PERSONAJES");
+            $(this).text("PERSONAJES");
         }
         let casillas = $(".casilla").toArray();
         for(var i = 0; i < 9; i++){
@@ -124,28 +102,32 @@ $(document).ready(function(){
             // poner color de letra transparente
             $(".casilla").css("color" , "transparent");
         }
-        p1=[];
-        p2=[];
+        p1=[];// NO juntar
+        p2=[];// NO juntar
         turno = 1;
+        $(".nombrePersonaje").removeAttr('style');
         $("#player1").removeClass("winner");
         $("#player2").removeClass("winner");
         $("#player1").css("height" , "100px");
         $("#player2").css("height" , "100px");
-        
+        $(".personaje").next().removeAttr('style');
+        $(perdedor).removeClass("oculto");
     });
 // Animacion Footer
-    $("footer").on({
+    $(".footer").on({
         mouseenter: function(){
-            $(this).animate({height: '10vh'}, 1000);
-            $(".disclaimer").animate({fontSize:'3rem'}, 1000,
+            $(this).animate({height: '10vh'}, 750);
+            $(".disclaimer").animate({fontSize:'3rem'}, 750,
                 function(){
                     setTimeout(function(){
-                        $("footer").animate({height: '4vh'}, 800);
-                        $(".disclaimer").animate({fontSize:'0rem'}, 1000);
+                        $(".footer").animate({height: '4vh'}, 750);
+                        $(".disclaimer").animate({fontSize:'0rem'}, 750);
+                        $(this).stop(true,true); // Detener animaciones que pudiesen encolarse
                     }
-                    ,3000);
+                    ,1500);
                 }
             );
         }
     });
 });
+
--- js/imagenes.js
+++ js/imagenes.js
@@ -1,42 +1,102 @@
-//variables para imagenes a seleccionar-----------------------------
-const goku=document.querySelector("[alt=Goku]");
-const imggoku1=document.createElement('img');
-const imggoku2=document.createElement('img');
+$(document).ready(function(){
+    let positionName, positionPersonaje;
+    let personajeCom;
 
-const veg=document.querySelector("[alt=Vegeta]");
-const imgveg1=document.createElement('img');
-const imgveg2=document.createElement('img');
+    function cambio(){
+        $("#personajes").addClass("oculto");
+        $("#zonaJuego").removeClass("oculto");
+    }
+    
+    function seleccionado(){
+        $(".personaje").animate({height: '0', width: '0'},1000,
+            function(){
+                cambio();
+                $(this).removeAttr('style');
+            }
+        );
+        $(".personaje").next().fadeOut();
+    }
 
-const goh=document.querySelector("[alt=Gohan]");
-const imggoh1=document.createElement('img');
-const imggoh2=document.createElement('img');
+    function selectPersonajeCom(personajePlayer){
+        do{
+            personajeCom = Math.round((Math.random()*2)+1);
+            console.log(personajeCom);
+        }while(personajePlayer == personajeCom);
+        return personajeCom;
+    }
 
-imggoku1.src=goku.src;
-imggoku2.setAttribute('src',
-'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/114004af-e326-47b9-9c34-4403b40fac33/d4r27wh-520dc229-2a36-4c08-99b2-090484e0dced.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzExNDAwNGFmLWUzMjYtNDdiOS05YzM0LTQ0MDNiNDBmYWMzM1wvZDRyMjd3aC01MjBkYzIyOS0yYTM2LTRjMDgtOTliMi0wOTA0ODRlMGRjZWQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.fs470Iur1s9D3UhQe3JpzE3e8aDsC_f9ksmY-YbiMyQ');
+// Se selecciona al personaje y pasamos al juego
+    $(".personaje").on({
+        click: function(){
+            let personajePlayer;
+            // enviar imagen correspondiente a la selección
+            switch($(this).attr("alt")){
+                case "Goku":
+                    $("#player1 img").attr("src", "./images/player-goku.png");
+                    personajePlayer = 1;
+                    break;
+                case "Vegeta":
+                    $("#player1 img").attr("src", "./images/player-vegeta.png");
+                    personajePlayer = 2;
+                    break;
+                case "Gohan":
+                    $("#player1 img").attr("src", "./images/player-gohan.png");
+                    personajePlayer = 3;
+                    break;
+            }
+            switch (selectPersonajeCom(personajePlayer)){
+                case 1:
+                    $("#player2 img").attr("src", "./images/player-goku.png");
+                    break;
+                case 2:
+                    $("#player2 img").attr("src", "./images/player-vegeta.png");
+                    break;
+                case 3:
+                    $("#player2 img").attr("src", "./images/player-gohan.png");
+                    break;
+            }
+            // animación de selección
+            seleccionado();
+        },
+        mouseenter: function(){
+            positionPersonaje = $(this).position();
+            positionName = $(this).next().position();
 
-imgveg1.src=veg.src;
-imgveg2.setAttribute('src',
-'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4575f0f4-2d97-4410-bbf9-25b2c1d08f31/db5csw7-4ad26c05-c409-4e7e-83a7-fa71e9dd247d.png/v1/fill/w_1024,h_2615,strp/majin_vegeta_by_koku78_db5csw7-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjYxNSIsInBhdGgiOiJcL2ZcLzQ1NzVmMGY0LTJkOTctNDQxMC1iYmY5LTI1YjJjMWQwOGYzMVwvZGI1Y3N3Ny00YWQyNmMwNS1jNDA5LTRlN2UtODNhNy1mYTcxZTlkZDI0N2QucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.AAInrmgTLN-OyZqUOl6PbMwOSFJMzxBuvOfOQz40rUw')
+            $(this).next().children()
+                .css({"position" : "absolute"});
 
-imggoh1.src=goh.src;
-imggoh2.setAttribute('src',
-'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2cb55797-9274-4f5f-a8d4-3aab8f3f9255/d33szss-43ab5cf9-aff9-4105-a72d-308f4fd7017f.png/v1/fill/w_400,h_613,strp/gohan_ssj_z_by_emiyansaiyan_d33szss-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjEzIiwicGF0aCI6IlwvZlwvMmNiNTU3OTctOTI3NC00ZjVmLWE4ZDQtM2FhYjhmM2Y5MjU1XC9kMzNzenNzLTQzYWI1Y2Y5LWFmZjktNDEwNS1hNzJkLTMwOGY0ZmQ3MDE3Zi5wbmciLCJ3aWR0aCI6Ijw9NDAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.X3nHbUoVySZCS3e7FtpC9Fzs4knobY0MdVphRUnFTdA')
-//------------------------------------------------------------------
+            $(this).animate({height: '100%'}, 500, "swing",
+                    function(){
+                        $(this).stop(true,false); // Detener animaciones que pudiesen encolarse
+                    }
+            );
 
-//imagen Goku
-goku.onmouseover=function(){
-    goku.src=imggoku2.src;
-};
-goku.onmouseleave=function(){goku.src=imggoku1.src;};
-//imagen Vegeta
-veg.onmouseover=function(){
-    veg.src=imgveg2.src;
-};
-veg.onmouseleave=function(){veg.src=imgveg1.src;};
-//imagen Gohan
-goh.onmouseover=function(){
-    goh.src=imggoh2.src;
-};
-goh.onmouseleave=function(){goh.src=imggoh1.src;};
-//------------------------------------------------------------------
+            $(this).next().children()
+                .animate({top : positionPersonaje.top-30,
+                        left : positionPersonaje.left,
+                        fontSize: "5rem",}, 500,
+                function(){
+                    $(this).stop(true,false); // Detener animaciones que pudiesen encolarse
+                }
+            );
+        },
+        mouseleave: function(){
+            
+            $(this).animate({height: '90%'},500,
+                    function(){
+                        $(this).removeAttr('style');
+                    }
+            );
+            
+            $(this).next().children()
+                .animate({top : positionName.top,
+                        left : positionName.left,
+                        fontSize: "0rem"}, 500,
+                    function(){
+                        $(this).stop(true,true); // Detener animaciones que pudiesen encolarse
+                        $(this).removeAttr('style'); // Remueve atributos para resetear encasillado
+                    }
+            );
+        }
+    });
+});
